596ce6190b17b7d6c8fb5c3507d54afe5a8ab4e9
mcfanta <johannes.theigen@gmail.com>
2016-09-16 23:00:09 +0200

    One small change

--- project/main.py
+++ project/main.py
@@ -4,7 +4,7 @@ def ball8():
     import random
 
     possible_answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely',
-                        'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes'                                                                        'Sign point to yes',
+                        'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Sign point to yes',
                         'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now',
                         'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no'
                         'Outlook not so good', 'Very doubtful']
